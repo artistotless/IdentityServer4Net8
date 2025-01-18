9ce70a2cd5be28ed87c0e25d51b536ebf97da449
a.boziev <a.boziev@logareon.ru>
2025-01-18 22:38:59 +0300

    init

---
+++ IdentityServer4/IdentityServer4.AspNetIdentity/IdentityServerBuilderExtensions.cs
@@ -0,0 +1,98 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.AspNetIdentity;
+using IdentityServer4.Test;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extension methods to add ASP.NET Identity support to IdentityServer.
+    /// </summary>
+    public static class IdentityServerBuilderExtensions
+    {
+        /// <summary>
+        /// Configures IdentityServer to use the ASP.NET Identity implementations 
+        /// of IUserClaimsPrincipalFactory, IResourceOwnerPasswordValidator, and IProfileService.
+        /// Also configures some of ASP.NET Identity's options for use with IdentityServer (such as claim types to use
+        /// and authentication cookie settings).
+        /// </summary>
+        /// <typeparam name="TUser">The type of the user.</typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IIdentityServerBuilder AddAspNetIdentity<TUser>(this IIdentityServerBuilder builder)
+            where TUser : class
+        {
+            builder.Services.AddTransientDecorator<IUserClaimsPrincipalFactory<TUser>, UserClaimsFactory<TUser>>();
+
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                options.ClaimsIdentity.UserIdClaimType = JwtClaimTypes.Subject;
+                options.ClaimsIdentity.UserNameClaimType = JwtClaimTypes.Name;
+                options.ClaimsIdentity.RoleClaimType = JwtClaimTypes.Role;
+            });
+
+            builder.Services.Configure<SecurityStampValidatorOptions>(opts =>
+            {
+                opts.OnRefreshingPrincipal = SecurityStampValidatorCallback.UpdatePrincipal;
+            });
+
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.IsEssential = true;
+                // we need to disable to allow iframe for authorize requests
+                options.Cookie.SameSite = AspNetCore.Http.SameSiteMode.None;
+            });
+
+            builder.Services.ConfigureExternalCookie(options =>
+            {
+                options.Cookie.IsEssential = true;
+                // https://github.com/IdentityServer/IdentityServer4/issues/2595
+                options.Cookie.SameSite = AspNetCore.Http.SameSiteMode.None;
+            });
+
+            builder.Services.Configure<CookieAuthenticationOptions>(IdentityConstants.TwoFactorRememberMeScheme, options =>
+            {
+                options.Cookie.IsEssential = true;
+            });
+
+            builder.Services.Configure<CookieAuthenticationOptions>(IdentityConstants.TwoFactorUserIdScheme, options =>
+            {
+                options.Cookie.IsEssential = true;
+            });
+
+            builder.Services.AddAuthentication(options =>
+            {
+                if (options.DefaultAuthenticateScheme == null &&
+                    options.DefaultScheme == IdentityServerConstants.DefaultCookieAuthenticationScheme)
+                {
+                    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                }
+            });
+
+            builder.AddResourceOwnerValidator<ResourceOwnerPasswordValidator<TUser>>();
+            builder.AddProfileService<ProfileService<TUser>>();
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds test users.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="users">The users.</param>
+        /// <returns></returns>
+        public static IIdentityServerBuilder AddTestUsers(this IIdentityServerBuilder builder, List<TestUser> users)
+        {
+            builder.Services.AddSingleton(new TestUserStore(users));
+            builder.AddProfileService<TestUserProfileService>();
+            builder.AddResourceOwnerValidator<TestUserResourceOwnerPasswordValidator>();
+
+            return builder;
+        }
+    }
+}
